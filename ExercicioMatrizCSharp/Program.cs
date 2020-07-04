5817ed073e7b2a02e809a2726b1c3285eff59c08
Juan Ormelli <juan_ormelli@hotmail.com>
2020-07-03 21:10:47 -0300

    Test For Worker Contract

---
+++ ExercicioMatrizCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioMatrizCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
